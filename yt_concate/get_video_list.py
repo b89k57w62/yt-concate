21a54bb8b5068581be0e2007c2758593c4ab02fc
“b89k57w62” <wu19930628@gmail.com>
2022-12-20 13:58:29 +0800

    3

---
+++ yt_concate/get_video_list.py
@@ -0,0 +1,35 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+import urllib.request
+import json
+
+from step import Step
+from setting import API_KEY
+class GetVideoList(Step):
+    def Process(self, data, inputs):
+        channel_id = inputs['channel_id']
+        
+
+        base_video_url = 'https://www.youtube.com/watch?v='
+        base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+        first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY, channel_id)
+
+        video_links = []
+        url = first_url
+        while True:
+            inp = urllib.request.urlopen(url)
+            resp = json.load(inp)
+
+            for i in resp['items']:
+                if i['id']['kind'] == "youtube#video":
+                    video_links.append(base_video_url + i['id']['videoId'])
+
+            try:
+                next_page_token = resp['nextPageToken']
+                url = first_url + '&pageToken={}'.format(next_page_token)
+            except KeyError as e:
+                print('exception happened')
+                break
+        print(len(video_links))
+        return video_links
--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,39 +1,20 @@
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-import urllib.request
-import json
-import os
-from setting import API_KEY
-
-
-#print(API_KEY)
-
+from get_video_list import GetVideoList
+from step import StepException
+from pipeline import Pipeline
 CHANNEL_ID = 'UCt_NLJ4McJlCyYM-dSPRo7Q'
-def get_all_video_in_channel(channel_id):
+def main():
+    steps = [
+        GetVideoList(),
+    ]
+
+    inputs = {        
+        'channel_id' : CHANNEL_ID
+        }
+    p = Pipeline(steps)
+    p.run(inputs)
+
+
+if __name__ == '__main__':
+    main()
     
-
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
-
-    first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY, channel_id)
-
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except:
-            break
-    return video_links
-video_list = get_all_video_in_channel(CHANNEL_ID)
-
-print(len(video_list))
 
